c1322732f0839ea7a95b794134877670bd84df4d
laurenmlang <laurenmlang@gmail.com>
2015-07-26 12:00:39 -0500

    First Draft

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -5,5 +5,28 @@ class GameController < ApplicationController
   end
 
   # Your code goes below.
+  def random_move
+  	return ["rock", "paper", "scissors"].sample
+  end
+
+  def rps
+  	@the_move = params["the_move"]
+
+  	@computer_move = random_move
+
+  	if @the_move == @computer_move
+  		@outcome = "tied"
+  	elsif @the_move == "rock" && @computer_move == "scissors"
+  		@outcome = "won"
+  	elsif @the_move == "rock" && @computer_move == "paper"
+  		@outcome = "lost"
+  	elsif @the_move == "paper" && @computer_move == "scissors"
+  		@outcome = "lost"	
+  	elsif @the_move == "paper" && @computer_move == "rock"
+  		@outcome = "won"  				
+  	end
+
+  	render("rps.html.erb")
+  end
 
 end
